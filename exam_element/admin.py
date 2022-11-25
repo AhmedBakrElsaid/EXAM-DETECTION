25a1a821366ca0f40317b879224b842582024293
AhmedBakrElsaid <Ahmedbakr24@std.mans.edu.eg>
2022-11-25 21:03:25 +0200

    add user models

--- exam_element/admin.py
+++ exam_element/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Account,Exam,Question,StudentAnswer,ClassRoom
 # Register your models here.
+
+admin.site.register(Account)
+admin.site.register(Exam)
+admin.site.register(Question)
+admin.site.register(StudentAnswer)
+admin.site.register(ClassRoom)
--- exam_element/models.py
+++ exam_element/models.py
@@ -1,3 +1,37 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+TYPE_OPTION =(
+    ('Teacher', 'Teacher'),
+    ('Student', 'Student'),
+)
+
+
+
+class Account(models.Model) :
+    
+    user = models.ForeignKey(User,related_name='user',on_delete=models.CASCADE)
+    account_type = models.CharField(max_length=50,choices=TYPE_OPTION)
+
+
+
+
+class ClassRoom (models.Model):
+    pass
+
+
+
+class Exam (models.Model):
+    pass
+
+
+
+class StudentAnswer (models.Model):
+    pass
+
+
+
+
+class Question (models.Model):
+    pass
