b8224c0668df04e07c7810c970a0b8e4d693e38c
Khaled Emad <khaledemad656@gmail.com>
2022-11-26 21:26:43 +0200

    edited models

--- exam_element/models.py
+++ exam_element/models.py
@@ -1,46 +1,52 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
 
 TYPE_OPTION =(
     ('Teacher', 'Teacher'),
     ('Student', 'Student'),
 )
 
-
-
 class Account(models.Model) :
-    
-    user = models.ForeignKey(User,related_name='user',on_delete=models.CASCADE)
-    account_type = models.CharField(max_length=50,choices=TYPE_OPTION)
-    student_id = models.ForeignKey('ClassRoom',related_name='student_id',on_delete=models.CASCADE)
+    user = models.OneToOneField(User, related_name='user', on_delete=models.CASCADE)
+    account_type = models.CharField(max_length=50, choices=TYPE_OPTION)
+    # classroom = models.ForeignKey('ClassRoom', related_name='student_id', on_delete=models.CASCADE)
+    classroom = models.ManyToManyField('ClassRoom', related_name='classrooms', blank=True)
+
+    def __str__(self) -> str:
+        return self.user.username
+
+class ClassRoom(models.Model):
+    # add validator it is a teacher who created the classroom
+    teacher_id = models.OneToOneField(Account, related_name='teacher_id', on_delete=models.CASCADE)
+    name= models.CharField(max_length=255, default='classroom')
+
+    def __str__(self) -> str:
+        return self.name
 
 
+class Exam(models.Model):
+    teacher = models.ForeignKey(Account, related_name="exams", on_delete=models.CASCADE)
+    classroom = models.ForeignKey(ClassRoom, related_name='classroom', on_delete=models.CASCADE)
+    name= models.CharField(max_length=255, default='classroom')
+
+    def __str__(self) -> str:
+        return self.name
 
 
-class ClassRoom (models.Model):
-    
-    teacher_id = models.ManyToManyField(Account, related_name='teacher_id')
-
-
-
-
-class Exam (models.Model):
-    exam_id = models.ForeignKey(ClassRoom,related_name='exam_id',on_delete=models.CASCADE)
-
-
-
-class StudentAnswer (models.Model):
-    student_answer = models.ForeignKey('Question',related_name='student_answer',on_delete=models.SET_NULL,null=True,blank=True)
-    answer = models.TextField()
-    std_answer = models.ForeignKey(Account,related_name='std_answer',on_delete=models.CASCADE)
-    
-
-
-
-
-class Question (models.Model):
-    questions = models.TextField()
-    question_re = models.ForeignKey(Exam,related_name='question',on_delete=models.CASCADE)
+class Question(models.Model):
+    exam = models.ForeignKey(Exam, related_name='questions', on_delete=models.CASCADE)
+    question = models.TextField()
     model_answer = models.TextField()
+
+    def __str__(self) -> str:
+        return self.question
+
+
+class StudentAnswer(models.Model):
+    student = models.ForeignKey(Account, related_name='std_answer', on_delete=models.CASCADE)
+    question = models.ForeignKey('Question', related_name='student_answer', on_delete=models.SET_NULL, null=True, blank=True)
+    answer = models.TextField()
+
+    def __str__(self) -> str:
+        return self.answer
     
