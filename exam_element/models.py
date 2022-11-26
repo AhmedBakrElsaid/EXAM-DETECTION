79c9b815375a4914aa5d48c9d071c6185296f18d
AhmedBakrElsaid <Ahmedbakr24@std.mans.edu.eg>
2022-11-26 18:19:20 +0200

    add some features to models

--- exam_element/models.py
+++ exam_element/models.py
@@ -13,17 +13,20 @@ class Account(models.Model) :
     
     user = models.ForeignKey(User,related_name='user',on_delete=models.CASCADE)
     account_type = models.CharField(max_length=50,choices=TYPE_OPTION)
+    student_id = models.ForeignKey('ClassRoom',related_name='student_id',on_delete=models.CASCADE)
 
 
 
 
 class ClassRoom (models.Model):
-    pass
+    
+    teacher_id = models.ManyToManyField(Account, related_name='teacher_id', on_delete = models.SET_NULL,blank=True,null=True)
+
 
 
 
 class Exam (models.Model):
-    pass
+    exam_id = models.ForeignKey(ClassRoom,related_name='exam_id',on_delete=models.CASCADE)
 
 
 
@@ -34,4 +37,4 @@ class StudentAnswer (models.Model):
 
 
 class Question (models.Model):
-    pass
+    question = models.ForeignKey(Exam,related_name='question',on_delete=models.CASCADE)
